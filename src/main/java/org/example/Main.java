2a8713dfe16bcfee472a3da3ead3034f58f4b198
Sakshi Agarwal <sakshiagarwal@Sakshis-MacBook-Air.local>
2026-04-19 16:45:21 +0530

    Create, execute, process the results and close connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,10 +22,30 @@ public class Main {
             final String url = "jdbc:postgresql://localhost:5432/spring-boot-jdbc";
             final String userName = "postgres";
             final String password = "0000";
+            final String sql = "select name from student where sid = 104";
 
             //Class.forName("org.postgresql.Driver"); //this is load and register - this is optional now as mentioned it's automatically done after java 6.
             Connection con = DriverManager.getConnection(url, userName, password);
             System.out.println("Connected to the PostgreSQL server successfully");
+
+            //create statement
+            Statement st = con.createStatement();
+
+            //execute statement
+            ResultSet resultSet = st.executeQuery(sql);
+
+            //process the results
+            boolean isDataExist = resultSet.next(); //next needs to be called as pointer to data is not at row 0 but before it, so to fetch even first row, have to first do next.
+            String printSt = isDataExist ? "Data exist" : "Data not exist";
+            System.out.println(printSt);
+
+            String name = resultSet.getString("name");
+            System.out.println("Name of student with Id 104 is : " + name);
+
+            //close the connection
+            con.close();
+            System.out.println("Connection closed");
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
